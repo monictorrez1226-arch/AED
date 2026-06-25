00b473f61132afedbd31e139f63c8d738e4cb196
monictorrez1226-arch <monictorrez1226@gmail.com>
2026-06-25 17:14:48 -0600

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-<iframe class="wp-embedded-content" sandbox="allow-scripts" security="restricted"
-	title="" - Frexus" src="" width="600" height="200" frameborder="0" marginwidth="0"
-	marginheight="0" scrolling="no"></iframe>
